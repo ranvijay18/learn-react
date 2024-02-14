28d4a47a88a95a76335354bc0ccf81b2c2247742
Ranvijay Singh <109961637+ranvijay18@users.noreply.github.com>
2024-02-14 15:50:26 +0530

    new

---
+++ expense-react/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Expenses from './components/Expenses';
+
+function App() {
+  return (
+   <>
+   <h1>Expenses Lists</h1>
+   <Expenses />
+   </>
+  );
+}
+
+export default App;
---
+++ expense-react/src/components/Expenses.js
@@ -0,0 +1,37 @@
+function Expenses(){
+    return(
+        <>
+        <div>
+            18 Feb 2023
+        </div>
+        <div>
+            <h1>Food</h1>
+        </div>
+        <div>
+            Rs. 10
+        </div>
+
+        <div>
+            18 Feb 2023
+        </div>
+        <div>
+            <h1>Petrol</h1>
+        </div>
+        <div>
+            Rs. 100
+        </div>
+
+        <div>
+            18 Feb 2023
+        </div>
+        <div>
+            <h1>Movie</h1>
+        </div>
+        <div>
+            Rs. 200
+        </div>
+        </>
+    )
+}
+
+export default Expenses;
